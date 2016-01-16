3f7e5a73483ef4f8fda3b2786ba5d8e4d56aaa7f
Max Titov <makstitoff@gmail.com>
2016-01-16 21:09:41 +0200

    del

--- gwt/src/com/mySampleApplication/client/LoginServiceAsync.java
+++
@@ -1,7 +0,0 @@
-package com.mySampleApplication.client;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface LoginServiceAsync {
-    void checkLoggedIn(AsyncCallback<Boolean> async);
-}
